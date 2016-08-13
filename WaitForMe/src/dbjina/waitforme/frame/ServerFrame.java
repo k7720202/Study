89e5e05b6e73acf938e3a197b60b5adef4abd83c
Jin <Jin@DESKTOP-GMDODKS>
2016-08-13 23:54:07 +0900

    Second time

--- WaitForMe/src/dbjina/waitforme/frame/ServerFrame.java
+++ WaitForMe/src/dbjina/waitforme/frame/ServerFrame.java
@@ -1,10 +1,18 @@
 package dbjina.waitforme.frame;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import dbjina.waitforme.property.ServerFrameProperty;
 
 public class ServerFrame extends JFrame{
+	JPanel stuPanel;
+	JButton stuButton;
+	
 	public ServerFrame() {
 		init();
 	}
@@ -13,6 +21,15 @@ public class ServerFrame extends JFrame{
 		setTitle(ServerFrameProperty.TITLE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+		setLayout(new BorderLayout());
+		
+		stuPanel = new JPanel();
+		stuPanel.setLayout(new GridLayout(5, 5));
+		
+		stuButton = new JButton("Test");
+		stuPanel.add(stuButton);
+		
+		add(stuPanel, BorderLayout.CENTER);
 		
 		setVisible(true);
 	}
